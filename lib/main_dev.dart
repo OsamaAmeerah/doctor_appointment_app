0329dd3fa478efdcffe715389b725112507a5ae1
OsamaAmeerah <osamaameera32@gmail.com>
2024-11-08 21:41:49 +0300

    - added launch.json for flavors config. - added dev and prod flavors for android. - changed android app name for each flavor. - added two main files for each flavor

---
+++ lib/main_dev.dart
@@ -0,0 +1,14 @@
+import 'package:advanced_course/core/di/dependency_injection.dart';
+import 'package:advanced_course/core/routing/app_router.dart';
+import 'package:advanced_course/doc_app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() async {
+  setupGetIt();
+  //* To fix font display issue in release mode, set up font scale */
+  await ScreenUtil.ensureScreenSize();
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  ));
+}
